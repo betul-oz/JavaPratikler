5a2038ee79680e135cd37c7b17fb9ea557664c4c
Betul Ozkan <ozkanb893@gmail.com>
2023-01-08 13:14:52 +0300

    Update ust.java

--- ust.java
+++ ust.java
@@ -7,8 +7,11 @@ public class ust {
     int k ;
     k = input.nextInt();
 
-    for (int i = 1; i <= k; i *= 2){
+    for (int i = 1; i <= k; i *= 4){
         System.out.println(i);
+    }
+    for (int j = 1; j <= k; j *= 5){
+        System.out.println(j);
     }
    }
 }

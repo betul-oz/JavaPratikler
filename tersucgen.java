c504bf14ad5a919941d7dc67307e8127ba536677
Betul Ozkan <ozkanb893@gmail.com>
2023-01-14 17:09:25 +0300

    Create tersucgen.java

---
+++ tersucgen.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class tersucgen {
+   public static void main(String[]args){
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Lütfen bir sayı giriniz: ");
+     int number = input.nextInt();
+  
+     for (int i=0; i<number; i++){
+
+        for (int j=0;j<=i;j++){
+            System.out.print(" ");
+     }
+
+        for (int k = 2*number-1; k >= 2*i+1; k--) {
+        System.out.print("*");
+     }      
+        System.out.println();
+  }
+ }
+}

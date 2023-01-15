3e151e560830e323a9a1d508675452e7595b429b
Betul Ozkan <ozkanb893@gmail.com>
2023-01-15 14:18:44 +0300

    Create main.java

---
+++ Bletter/main.java
@@ -0,0 +1,27 @@
+package Bletter;
+
+ public class main {
+    public static void main(String[] args) {
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i <= letter.length; i++){
+            for (int j = 0; j <= letter[i].length; j++){
+                if (i == 0 || i == 3 || i == 6) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+ 
+        for (int i = 0;i < letter.length;i++){
+            for(int j = 0; j < letter[i].length;j++){
+                System.out.print(letter[i][j]);
+            }
+            System.out.println();
+        }
+ 
+    }
+}
